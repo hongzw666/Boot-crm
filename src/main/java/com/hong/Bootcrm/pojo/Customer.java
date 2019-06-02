7d027d5a2cfcd5bb07ca67ee69cff1574b75cc18
pc <pc@DESKTOP-6AQEV2O>
2019-06-02 23:33:24 +0800

    pojo

---
+++ src/main/java/com/hong/Bootcrm/pojo/Customer.java
@@ -0,0 +1,97 @@
+package com.hong.Bootcrm.pojo;
+
+public class Customer {
+	private String custId;
+	private String custName;
+	private String custUserId;
+	private String custCreateId;
+	private String custSource;
+	private String custIndustry;
+	private String custevel;
+	private String custinkman;
+	private String custPhone;	
+	private String custMobile;
+	private String custZipcode;
+	private String custAddreess;
+	private String custCreatetime;
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCustUserId() {
+		return custUserId;
+	}
+	public void setCustUserId(String custUserId) {
+		this.custUserId = custUserId;
+	}
+	public String getCustCreateId() {
+		return custCreateId;
+	}
+	public void setCustCreateId(String custCreateId) {
+		this.custCreateId = custCreateId;
+	}
+	public String getCustSource() {
+		return custSource;
+	}
+	public void setCustSource(String custSource) {
+		this.custSource = custSource;
+	}
+	public String getCustIndustry() {
+		return custIndustry;
+	}
+	public void setCustIndustry(String custIndustry) {
+		this.custIndustry = custIndustry;
+	}
+	public String getCustevel() {
+		return custevel;
+	}
+	public void setCustevel(String custevel) {
+		this.custevel = custevel;
+	}
+	public String getCustinkman() {
+		return custinkman;
+	}
+	public void setCustinkman(String custinkman) {
+		this.custinkman = custinkman;
+	}
+	public String getCustPhone() {
+		return custPhone;
+	}
+	public void setCustPhone(String custPhone) {
+		this.custPhone = custPhone;
+	}
+	public String getCustMobile() {
+		return custMobile;
+	}
+	public void setCustMobile(String custMobile) {
+		this.custMobile = custMobile;
+	}
+	public String getCustZipcode() {
+		return custZipcode;
+	}
+	public void setCustZipcode(String custZipcode) {
+		this.custZipcode = custZipcode;
+	}
+	public String getCustAddreess() {
+		return custAddreess;
+	}
+	public void setCustAddreess(String custAddreess) {
+		this.custAddreess = custAddreess;
+	}
+	public String getCustCreatetime() {
+		return custCreatetime;
+	}
+	public void setCustCreatetime(String custCreatetime) {
+		this.custCreatetime = custCreatetime;
+	}
+	
+}
+
---
+++ src/main/java/com/hong/Bootcrm/pojo/SysUser.java
@@ -0,0 +1,40 @@
+package com.hong.Bootcrm.pojo;
+
+public class SysUser {
+	
+	private String userId;	
+	private String userCode;
+	private String userName;	
+	private String userPassword;
+	private String userState;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getUserState() {
+		return userState;
+	}
+	public void setUserState(String userState) {
+		this.userState = userState;
+	}
+}
