391ebeb1cbbc3a34c23f476c75d880c040b87004
pc <pc@DESKTOP-6AQEV2O>
2019-06-03 10:11:44 +0800

    Sysservice

---
+++ src/main/java/com/hong/Bootcrm/controller/SysUserController.java
@@ -0,0 +1,29 @@
+package com.hong.Bootcrm.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hong.Bootcrm.pojo.SysUser;
+import com.hong.Bootcrm.service.SysUserService;
+
+@Controller
+public class SysUserController {
+	@Autowired
+	private SysUserService sysUserService;
+	
+	@RequestMapping("/login")
+	@ResponseBody
+	public String login(@RequestBody SysUser user,HttpSession session) {
+		
+		SysUser findUser = sysUserService.findUser( user.getUserCode(),user.getUserPassword());
+		if (findUser!=null) {
+			return "customer";
+		}
+		return "login.html";
+	} 
+}
--- src/main/java/com/hong/Bootcrm/service/SysUserService.java
+++ src/main/java/com/hong/Bootcrm/service/SysUserService.java
@@ -4,4 +4,6 @@ import com.hong.Bootcrm.pojo.SysUser;
 
 public interface SysUserService {
 	public SysUser findUser(String usercode,String password);
+
+	//public SysUser findUser(SysUser user);
 }
